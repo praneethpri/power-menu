ea9276700bc03ddfcf9f44de5f00b75b6c78bd03
Praneeth Priyankara <ndppriyankara@gmail.com>
2024-12-29 07:16:21 +0530

    Changes in the window and adding main function.

--- powermenu.py
+++ powermenu.py
@@ -1,14 +1,19 @@
-import tkinter
 import customtkinter
 
-customtkinter.set_appearance_mode('light')
+def main():
+    customtkinter.set_appearance_mode('light')
+    root = customtkinter.CTk()
+    root.title("Power Options")
+    root.geometry("600x200")
+    root.attributes('-topmost', True)
+    root.attributes('-type', 'dialog')
+    root.grid_rowconfigure(0, weight=1, minsize=50)
+    root.grid_columnconfigure(0, weight=1)
+    root.grid_columnconfigure(1, weight=1)
+    button_shutdown = customtkinter.CTkButton(master=root, text='Shutdown')
+    button_shutdown.grid(row=0, column=0, padx=1, pady=1)
+    button_restart = customtkinter.CTkButton(master=root, text='Restart')
+    button_restart.grid(row=0, column=1, padx=1, pady=1)
+    root.mainloop()
 
-root = customtkinter.CTk()
-
-root.geometry("300x400")
-
-button = customtkinter.CTkButton(master=root, text='Hello')
-
-button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-
-root.mainloop()
+main()
